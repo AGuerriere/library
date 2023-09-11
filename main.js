e2da9e524d5559c9ca48089564c8a9216f6673c5
Antonio Guerriere <antonioguerriere@gmail.com>
2023-09-11 22:44:28 +0100

    working draft

--- main.js
+++ main.js
@@ -1,4 +1,31 @@
-const myLibrary = [];
+const addBookButton = document.getElementById("addBookButton")
+const newBookFormContainer = document.querySelector(".formContainer")
+const newBookForm = document.querySelector("form")
+const newBook = document.getElementById("title")
+const newAuthor = document.getElementById("author")
+const newPageNumber = document.getElementById("pages")
+const readStatusCheckBox = document.getElementById("readStatus")
+const submitButton = document.getElementById("submit")
+const books = document.querySelector(".books")
+
+const myLibrary = [{
+  "title": "harry Potter and The Philosopher Stone",
+  "author": "JK Rowling",
+  "pages": "223",
+  "readStatus": true
+},
+{
+  "title": "21 Lessons for the 21st Century",
+  "author": "Yuval Noah Harari",
+  "pages": "416",
+  "readStatus": false
+},
+{
+  "title": "Casino Royale",
+  "author": "Ian Fleming",
+  "pages": "224",
+  "readStatus": true
+}];
 
 function Book(title, author, pages, readStatus) {
   this.title = title
@@ -7,17 +34,58 @@ function Book(title, author, pages, readStatus) {
   this.readStatus = readStatus
 }
 
-function addBookToLibrary(newBook) {
-  // do stuff here
+function updateLibary() {
+  // clear the library from the previous data
+  const bookItems = document.querySelectorAll(".bookItem");
+  bookItems.forEach((item) => {
+    item.remove();
+  });
+  // recalculate the library
+  for (const element of myLibrary) {
+    let bookItem = document.createElement("div")
+    console.log(element.title)
+    bookItem.classList = "bookItem"
+    const bookTitle = document.createElement("p")
+    const bookAuthor = document.createElement("p")
+    const bookPages = document.createElement("p")
+    const readingStatus = document.createElement("button")
+    books.append(bookItem)
+    bookItem.append(bookTitle)
+    bookItem.append(bookAuthor)
+    bookItem.append(bookPages)
+    bookItem.append(readingStatus)
+
+    bookTitle.innerHTML = element.title
+    bookAuthor.innerHTML = element.author
+    bookPages.innerHTML = element.pages
+    readingStatus.innerHTML = element.readStatus ? "Read" : "Not Read"
+    readingStatus.classList = element.readStatus ? "bookRead" : "bookNotRead"
+    bookItem.classList.add("bookItem")
+  }
 }
 
-const addBookButton = document.getElementById("addBookButton")
-const newBookForm = document.querySelector(".formContainer")
-const newBook = document.getElementById("title")
-const newAuthor = document.getElementById("author")
-const newPageNumber = document.getElementById("pages")
-const readStatusCheckBox = document.getElementById("readStatus")
+function addBookToLibrary(bookTitle) {
+  myLibrary.push(new Book(newBook.value, newAuthor.value, newPageNumber.value, readStatusCheckBox.checked))
+}
 
-addBookButton.addEventListener("click", ()=> {
-  newBookForm.classList.toggle("active")
+
+addBookButton.addEventListener("click", () => {
+  newBookFormContainer.classList.toggle("active");
+  const overlay = document.getElementById("overlay");
+  overlay.style.display = "block"
+  overlay.addEventListener("click", () => {
+    overlay.style.display = "none"
+    newBookFormContainer.classList.remove("active");
+  })
+});
+
+submitButton.addEventListener("click", (e) =>  {
+  addBookToLibrary(newBook.value)
+  newBookFormContainer.classList.toggle("active")
+  updateLibary()
+  overlay.style.display = "none"
+  newBookForm.reset()
 })
+
+
+updateLibary()
