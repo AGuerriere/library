41d3177d2cb634de2912acd05b12d9cc4a259618
Antonio Guerriere <antonioguerriere@gmail.com>
2023-09-12 10:48:26 +0100

    added delete button and more style

--- main.js
+++ main.js
@@ -1,91 +1,132 @@
-const addBookButton = document.getElementById("addBookButton")
-const newBookFormContainer = document.querySelector(".formContainer")
-const newBookForm = document.querySelector("form")
-const newBook = document.getElementById("title")
-const newAuthor = document.getElementById("author")
-const newPageNumber = document.getElementById("pages")
-const readStatusCheckBox = document.getElementById("readStatus")
-const submitButton = document.getElementById("submit")
-const books = document.querySelector(".books")
+// Get DOM elements
+const addBookButton = document.getElementById("addBookButton");
+const newBookFormContainer = document.querySelector(".formContainer");
+const newBookForm = document.querySelector("form");
+const newBook = document.getElementById("title");
+const newAuthor = document.getElementById("author");
+const newPageNumber = document.getElementById("pages");
+const readStatusCheckBox = document.getElementById("readStatus");
+const submitButton = document.getElementById("submit");
+const books = document.querySelector(".books");
 
-const myLibrary = [{
-  "title": "harry Potter and The Philosopher Stone",
-  "author": "JK Rowling",
-  "pages": "223",
-  "readStatus": true
-},
-{
-  "title": "21 Lessons for the 21st Century",
-  "author": "Yuval Noah Harari",
-  "pages": "416",
-  "readStatus": false
-},
-{
-  "title": "Casino Royale",
-  "author": "Ian Fleming",
-  "pages": "224",
-  "readStatus": true
-}];
+// Sample book data
+const myLibrary = [
+  {
+    "title": "Harry Potter and The Philosopher Stone",
+    "author": "JK Rowling",
+    "pages": "223",
+    "readStatus": true
+  },
+  {
+    "title": "21 Lessons for the 21st Century",
+    "author": "Yuval Noah Harari",
+    "pages": "416",
+    "readStatus": false
+  },
+  {
+    "title": "Casino Royale",
+    "author": "Ian Fleming",
+    "pages": "224",
+    "readStatus": true
+  }
+];
 
+// Book constructor
 function Book(title, author, pages, readStatus) {
-  this.title = title
-  this.author = author
-  this.pages = pages
-  this.readStatus = readStatus
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.readStatus = readStatus;
 }
 
-function updateLibary() {
-  // clear the library from the previous data
+// Function to update the library display
+function updateLibrary() {
+  // Clear the library display
   const bookItems = document.querySelectorAll(".bookItem");
   bookItems.forEach((item) => {
     item.remove();
   });
-  // recalculate the library
-  for (const element of myLibrary) {
-    let bookItem = document.createElement("div")
-    console.log(element.title)
-    bookItem.classList = "bookItem"
-    const bookTitle = document.createElement("p")
-    const bookAuthor = document.createElement("p")
-    const bookPages = document.createElement("p")
-    const readingStatus = document.createElement("button")
-    books.append(bookItem)
-    bookItem.append(bookTitle)
-    bookItem.append(bookAuthor)
-    bookItem.append(bookPages)
-    bookItem.append(readingStatus)
 
-    bookTitle.innerHTML = element.title
-    bookAuthor.innerHTML = element.author
-    bookPages.innerHTML = element.pages
-    readingStatus.innerHTML = element.readStatus ? "Read" : "Not Read"
-    readingStatus.classList = element.readStatus ? "bookRead" : "bookNotRead"
-    bookItem.classList.add("bookItem")
+  // Recreate the library display based on the myLibrary array
+  for (const element of myLibrary) {
+    let bookItem = document.createElement("div");
+    bookItem.classList = "bookItem";
+    const bookTitle = document.createElement("p");
+    const bookAuthor = document.createElement("p");
+    const bookPages = document.createElement("p");
+    const readingStatus = document.createElement("button");
+    const deleteButton = document.createElement("button");
+
+    // Append elements to the DOM
+    books.append(bookItem);
+    bookItem.append(bookTitle);
+    bookItem.append(bookAuthor);
+    bookItem.append(bookPages);
+    bookItem.append(readingStatus);
+    bookItem.append(deleteButton);
+
+    // Set text and classes
+    bookTitle.innerText = element.title;
+    bookAuthor.innerText = element.author;
+    bookPages.innerText = `${element.pages} pages`;
+    deleteButton.innerText = "Remove";
+    deleteButton.classList = "deleteButton";
+    readingStatus.innerText = element.readStatus ? "Read" : "Not Read";
+    readingStatus.classList = element.readStatus ? "bookRead" : "bookNotRead";
+    bookItem.classList.add("bookItem");
+
+    // Add event listeners to toggle read status and delete book
+    readingStatus.addEventListener('click', () => {
+      if (readingStatus.classList.contains("bookRead")) {
+        readingStatus.classList.replace("bookRead", "bookNotRead");
+        element.readStatus = false;
+        readingStatus.innerText = "Not Read";
+      } else {
+        readingStatus.classList.replace("bookNotRead", "bookRead");
+        readingStatus.innerText = "Read";
+        element.readStatus = true;
+      }
+    });
+
+    deleteButton.addEventListener("click", () => {
+      const index = myLibrary.indexOf(element);
+      if (index > -1) {
+        myLibrary.splice(index, 1);
+      }
+      bookItem.remove();
+    });
   }
 }
 
+// Function to add a new book to the library
 function addBookToLibrary(bookTitle) {
-  myLibrary.push(new Book(newBook.value, newAuthor.value, newPageNumber.value, readStatusCheckBox.checked))
+  myLibrary.push(new Book(newBook.value, newAuthor.value, newPageNumber.value, readStatusCheckBox.checked));
 }
 
-
+// Event listeners
 addBookButton.addEventListener("click", () => {
   newBookFormContainer.classList.toggle("active");
   const overlay = document.getElementById("overlay");
-  overlay.style.display = "block"
+  overlay.style.display = "block";
   overlay.addEventListener("click", () => {
-    overlay.style.display = "none"
+    overlay.style.display = "none";
     newBookFormContainer.classList.remove("active");
-  })
+  });
 });
 
 submitButton.addEventListener("click", (e) =>  {
-  addBookToLibrary(newBook.value)
-  newBookFormContainer.classList.toggle("active")
-  updateLibary()
-  overlay.style.display = "none"
-  newBookForm.reset()
-})
+  if (!newBook.value && !newAuthor.value && !newPageNumber.value) {
+    const warning = document.createElement("p")
+    warning.classList.add("warning")
+    warning.innerText = "‼Complete all fields"
+    return
+  }
+  addBookToLibrary(newBook.value);
+  newBookFormContainer.classList.toggle("active");
+  updateLibrary(); // Update the library display
+  overlay.style.display = "none";
+  newBookForm.reset();
+});
 
-
-updateLibary()
+// Initial library display update
+updateLibrary();
